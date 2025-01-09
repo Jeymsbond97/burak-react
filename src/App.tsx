e4f61c80667a599cb1966bb5f32c66b3261423bf
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-01-09 23:29:05 +0500

    feat: develop burak screen based components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import {Box, Button, Container, Stack, Typography} from "@mui/material"
 import './css/app.css';
-import { RippleBadge } from './app/MaterialTheme/styled';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { HomePage } from './app/screens/homePage';
+import { ProductsPage } from './app/screens/productsPage';
+import { OrdersPage } from './app/screens/ordersPage';
+import { UserPage } from './app/screens/userPage';
 
 function App() {
   return (
-    <Container maxWidth = "sm">
-       <Stack flexDirection={"column"}>
-          <Box sx={{my: 4}}>
-            <Typography variant='h4' component={"h4"}>
-                Create react App on TypeScript with Redux
-            </Typography>
-          </Box>
-          <Box>
-            <RippleBadge badgeContent={4}>
-              <Button variant='contained'>Contained</Button>
-            </RippleBadge>
-          </Box>
-       </Stack>
-    </Container>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">HomePage</Link>
+            </li>
+            <li>
+              <Link to="/products">Productspage</Link>
+            </li>
+            <li>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">UserPage</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/products">
+            <ProductsPage />
+          </Route>
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <UserPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
