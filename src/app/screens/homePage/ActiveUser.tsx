874db67feb0437f135728c9fdb95821cb2f87034
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-01-16 07:04:42 +0900

    feat: screen homapage's pages created

---
+++ src/app/screens/homePage/ActiveUser.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function  ActiveUser() {
+    return (
+        <div>
+            <Container>
+                ActiveUser
+            </Container>
+        </div>
+    )
+}
