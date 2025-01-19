b435c4661fa6662202c3aa2d8d6767f48c276a4f
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-01-19 16:32:55 +0900

    feat: product and productChosen pages published

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -13,8 +13,8 @@ import SearchIcon from  "@mui/icons-material/Search"
 
 const  products = [
     {productName: "Cutlet", imagePath: "img/cutlet.webp"},
-    {productName: "Kebab", imagePath: "img/kabab-fresh.webp"},
-    {productName: "Kebab", imagePath: "img/kabab.webp"},
+    {productName: "Kebab", imagePath: "img/kebab-fresh.webp"},
+    {productName: "Kebab", imagePath: "img/kebab.webp"},
     {productName: "Lavash", imagePath: "img/lavash.webp"},
     {productName: "Lavash", imagePath: "img/lavash.webp"},
     {productName: "Cutlet", imagePath: "img/cutlet.webp"},
@@ -28,7 +28,17 @@ export default  function Products() {
             <Container>
                 <Stack flexDirection={"column"} alignItems={"center"}>
                     <Stack className="avatar-big-box">
-                        
+                        <Stack className="top-content">
+                            <Box className="text">Burak Restaurant</Box>
+                            <Box className="search-container">
+                                <input type="text"
+                                className="search-input"
+                                placeholder="type here" />
+                                <button className="search-button">
+                                    Search <SearchIcon/>
+                                </button>
+                            </Box>
+                        </Stack>
                     </Stack>
 
                     <Stack className="dishes-filter-section">
@@ -46,22 +56,22 @@ export default  function Products() {
                     </Stack>
 
                     <Stack className="list-category-section">
-                        <Stack className="{product-category">
+                        <Stack className="product-category">
                             <div className="category-main">
-                                <Button variant="contained" color="primary" className="order">
-                                    DISH
+                                <Button variant="contained" color="secondary" className="order">
+                                    OTHER
                                 </Button>
                                 <Button variant="contained" color ="secondary" className="order">
-                                    SALAD
+                                    DESERT
                                 </Button>
                                 <Button variant="contained" color="secondary" className="order">
                                     DRINK
                                 </Button>
                                 <Button variant="contained" color="secondary" className="order">
-                                    DESERT
+                                    SALAD
                                 </Button>
-                                <Button variant="contained" color="secondary" className="order">
-                                    OTHER
+                                <Button variant="contained" color="primary" className="order">
+                                    DISH
                                 </Button>
                             </div>
                         </Stack>
@@ -70,19 +80,19 @@ export default  function Products() {
                         {products.length !== 0 ? (
                             products.map((product, index) => {
                                 return (
-                                    <Stack key={index}>
+                                    <Stack key={index} className="product-card">
                                         <Stack className="product-img" sx={{background: `url(${product.imagePath})`}}>
                                             <div className="product-sale">Large size</div>
                                             <Button className="shop-btn">
                                                 <img src={"/icons/shopping-cart.svg"} alt="" style={{display:"flex"}} />
                                             </Button>
-                                            <Button>
+                                            <Button className="view-btn" sx={{right: "40px"}}>
                                                 <Badge badgeContent ={20} color="secondary">
                                                     <RemoveRedEyeIcon sx ={{color: 20 > 0 ? "gray" : "white",}} />
                                                 </Badge>
                                             </Button>
                                         </Stack>
-                                        <Box className="product-desc">
+                                        <Box className="product-descs">
                                             <span className="product-title">
                                                 {product.productName}
                                             </span>
@@ -116,13 +126,13 @@ export default  function Products() {
                             <img src={"/img/gurme.webp"} alt="" />
                         </Box>
                         <Box className="review-box">
-                            <img src={"/img/gurme.webp"} alt="" />
+                            <img src={"/img/seafood.webp"} alt="" />
                         </Box>
                         <Box className="review-box" >
-                            <img src={"/img/gurme.webp"} alt="" />
+                            <img src={"/img/doner.webp"} alt="" />
                         </Box>
                         <Box className="review-box">
-                            <img src={"/img/gurme.webp"} alt="" />
+                            <img src={"/img/sweets.webp"} alt="" />
                         </Box>
                     </Stack>
                 </Container>
@@ -133,7 +143,7 @@ export default  function Products() {
                     <Stack className="address-area">
                         <Box className="tittle">Our address</Box>
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127284.99733249089!2d127.02916684179687!3d35.8457338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357023430316d863%3A0xc7bc17e091c3429a!2sJeonju-si%2C%20Jeollabuk-do%2C%20South%20Korea!5e0!3m2!1sen!2sus!4v1700660482263!5m2!1sen!2sus"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.363734762081!2d69.2267250514616!3d41.322703307863044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b9a0a33281d%3A0x9c5015eab678e435!2z0KDQsNC50YXQvtC9!5e0!3m2!1sko!2skr!4v1655461169573!5m2!1sko!2skr"
                             width="1320"
                             height="500"
                             style={{ marginTop: "50px"}}
