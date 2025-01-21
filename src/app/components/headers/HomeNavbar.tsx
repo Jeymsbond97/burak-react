21a3b46b639e961d37078f0f721a70dd23a88d45
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-01-21 23:38:43 +0900

    fix: authMember state is changed

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -3,55 +3,55 @@ import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
 
 export default function HomeNavbar() {
-    const authMember = true;
+    const authMember = null;
     return (
         <div className="home-navbar">
-          <Container className="navbar-container" sx={{ mt: "55px", height: "642px" }}>
-            <Stack className="menu" >
-               <Box>
-                <NavLink to="/" className="custom-link">
-                    <img className="brand-logo" src="/icons/burak.svg" alt=""/>
-                </NavLink>
-               </Box>
-               <Stack className="links">
-                    <Box className={"hover-line"} >
-                        <NavLink to="/" className="custom-link" activeClassName="underline">
-                           Home
-                        </NavLink>
-                    </Box>
-                    <Box className={"hover-line"} >
-                        <NavLink to="/products" className="custom-link" activeClassName="underline">
-                           Products
-                        </NavLink>
-                    </Box>
-                    {authMember ? (
-                        <Box className={"hover-line"}>
-                            <NavLink to="/orders" className="custom-link" activeClassName="underline">
-                               Orders
+            <Container className="navbar-container" sx={{ mt: "55px", height: "642px" }}>
+                <Stack className="menu" >
+                <Box>
+                    <NavLink to="/" className="custom-link">
+                        <img className="brand-logo" src="/icons/burak.svg" alt=""/>
+                    </NavLink>
+                </Box>
+                <Stack className="links">
+                        <Box className={"hover-line"} >
+                            <NavLink to="/" className="custom-link" activeClassName="underline">
+                                Home
                             </NavLink>
-                    </Box>
-                    ) : null}
-                    {authMember ? (
-                        <Box className={"hover-line"}>
-                            <NavLink to="/member-page" className="custom-link" activeClassName="underline">
-                               My Page
-                            </NavLink>
-                    </Box>
-                    ) : null}
-                    <Box className={"hover-line"}>
-                        <NavLink to="/help" className="custom-link" activeClassName="underline">
-                           Help
-                        </NavLink>
-                    </Box>
-                     <Basket/>
-                    {!authMember ? (
-                        <Box>
-                            <Button variant="contained" className="login-button">Login</Button>
                         </Box>
-                    ) : (
-                        <img className="user-avatar" src={"/icons/default-user.svg"} alt="" aria-haspopup={"true"}/>
-                    )}
-               </Stack>
+                        <Box className={"hover-line"} >
+                            <NavLink to="/products" className="custom-link" activeClassName="underline">
+                            Products
+                            </NavLink>
+                        </Box>
+                        {authMember ? (
+                            <Box className={"hover-line"}>
+                                <NavLink to="/orders" className="custom-link" activeClassName="underline">
+                                Orders
+                                </NavLink>
+                        </Box>
+                        ) : null}
+                        {authMember ? (
+                            <Box className={"hover-line"}>
+                                <NavLink to="/member-page" className="custom-link" activeClassName="underline">
+                                My Page
+                                </NavLink>
+                        </Box>
+                        ) : null}
+                        <Box className={"hover-line"}>
+                            <NavLink to="/help" className="custom-link" activeClassName="underline">
+                            Help
+                            </NavLink>
+                        </Box>
+                        <Basket/>
+                        {!authMember ? (
+                            <Box>
+                                <Button variant="contained" className="login-button">Login</Button>
+                            </Box>
+                        ) : (
+                            <img className="user-avatar" src={"/icons/default-user.svg"} alt="" aria-haspopup={"true"}/>
+                        )}
+                </Stack>
             </Stack>
             <Stack className="header-frame">
                 <Stack className="detail">
@@ -59,7 +59,7 @@ export default function HomeNavbar() {
                         World's Most Delicious Cousine
                     </Box>
                     <Box className = "wel-txt">
-                      The Choice, not just a choice
+                        The Choice, not just a choice
                     </Box>
                     <Box className= "service-txt">
                         24 hours service
