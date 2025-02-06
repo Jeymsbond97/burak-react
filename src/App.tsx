c2bf4318ace6a9ceb45cfecf62305ea7677d2f80
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-02-06 14:19:52 +0900

    fix: modify homepage

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import  HelpPage  from './app/screens/helpPage';
 import './css/app.css';
 import "./css/navbar.css";
 import "./css/footer.css"
-import Test from './Test';
 
 
 function App() {
@@ -35,8 +34,7 @@ function App() {
             <HelpPage />
           </Route>
           <Route path="/">
-            {/* <HomePage /> */}
-            <Test/>
+            <HomePage />
           </Route>
         </Switch>
         <Footer/>
